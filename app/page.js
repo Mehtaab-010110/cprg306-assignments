7d687c87fa8155d890043a2f6169010890734535
Mehtab Singh <mehtab.singh@edu.sait.ca>
2023-11-14 19:09:09 -0700

    Changes made to the final draft

--- app/page.js
+++ app/page.js
@@ -29,6 +29,10 @@ export default function Home() {
         <Link href="/week6">
           Week 6
         </Link>
+        <br/>
+        <Link href="/week7">
+          Week 7
+        </Link>
 
       </div>
     </main>
---
+++ app/week7/item-list.js
@@ -0,0 +1,54 @@
+"use client";
+
+import React, { useState } from 'react';
+import Item from './item';
+
+function ItemList({ items, onItemSelect }) { // Accepting items as a prop
+    const [sortBy, setSortBy] = useState('name');  // Initializing sortBy to 'name'
+
+    // Sorting the items based on sortBy
+    const sortedItems = [...items];  // Using the items prop
+    if (sortBy === 'name') {
+        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortBy === 'category') {
+        sortedItems.sort((a, b) => a.category.localeCompare(b.category));
+    }
+
+    return (
+        <div>
+            <div className="mb-4">
+                <button 
+                    onClick={() => setSortBy('name')}
+                    className={`px-4 py-2 border rounded-md ${sortBy === 'name' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500 border-blue-500'}`}>
+                    Sort by Name
+                </button>
+
+                <button 
+                    onClick={() => setSortBy('category')}
+                    className={`ml-2 px-4 py-2 border rounded-md ${sortBy === 'category' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500 border-blue-500'}`}>
+                    Sort by Category
+                </button>
+            </div>
+
+            <ul>
+                {sortedItems.map(item => (
+                    <Item key={item.id} name={item.name} quantity={item.quantity} category={item.category} />
+                ))}
+            </ul>
+
+            <ul>
+                {items.map((item) => (
+                <Item 
+                    key={item.id} 
+                    name={item.name} 
+                    quantity={item.quantity} 
+                    category={item.category}
+                    onSelect={onItemSelect}  // Passing onSelect to each Item
+                />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ItemList;
